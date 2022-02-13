9c9fb3a63fa79b9e0c1438fb6227f273316607a5
Oleg <exaple@gmail.com>
2022-02-13 15:08:46 +0200

    Cycles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
 
 const obj = {
     count: numberOfFilms,
@@ -14,12 +14,29 @@ const obj = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
-obj.movies[a] = b;
-obj.movies[c] = d;
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length<50) {
+        obj.movies[a] = b;
+        console.log ('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+    if (obj.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (obj.count >= 10 && obj.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (obj.count >= 30) {
+        console.log("Вы киноман!");
+    } else {
+        console.log("Ошибка!");
+    }
 
 console.log(obj);
